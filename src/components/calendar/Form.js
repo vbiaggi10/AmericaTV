0919809a783a840e7a4f58cc2159d0ea39efc3b9
AlejandraAAO <aventurero_7_20@hotmail.com>
2018-10-31 04:17:46 -0500

    añadiendo funcionalidad de acumulacion de precios

--- src/components/calendar/Form.js
+++ src/components/calendar/Form.js
@@ -1,13 +1,17 @@
 import React, { Component } from 'react';
-import Mini from './mini'
+import Mini from './MiniCalendar'
 const database = window.firebase.database();
-
 class Form extends Component {
   constructor() {
     super()
     this.state = {
-      products: []
+      products: [],
+      productName: '',
+      productPrice: 0,
+      surchargeAmount: 0,
+      totalAmount: 0
     }
+    this.myRef = React.createRef();
   }
   componentDidMount() {
     database.ref('/agency').once('value').then(snapshot => {
@@ -16,61 +20,82 @@ class Form extends Component {
         products: arr
       })
     });
+    const hour = parseInt(this.props.hour);
+    if (hour >= 12 && hour < 16) {
+      this.setState({ surchargeAmount: (this.props.price * 0.05) });
+    } else if (hour >= 16 && hour < 20) {
+      this.setState({ surchargeAmount: (this.props.price * 0.15) });
+    } else {
+      this.setState({ surchargeAmount: 0 });
+    }
   }
   render() {
     return (
-      <div class="form-container">
-        <form className="form-data p-5">
+      <div>
+        <form className="form-data p-5" onSubmit={this.handleSubmit.bind(this)}>
           <Mini hour={this.props.hour} />
-          <div class="form-group">
-            <label for="exampleSelect1" class="bmd-label-floating">Producto</label>
-            <select class="form-control" id="exampleSelect1">
-              {this.state.products.map(element => {
-                return (<option>{element.name}</option>)
+          <div className="form-group">
+            <label htmlFor="exampleSelect1" className="bmd-label-floating">Producto</label>
+            <select className="form-control" id="exampleSelect1" onChange={this.handleChange.bind(this)}>
+              <option disabled selected>Selecciona una opción</option>
+              {this.state.products.map((element, i) => {
+                return (<option key={`products${i}`} value={element.price + "-" + element.name}>{element.name}</option>)
               })}
             </select>
           </div>
-          <div class="form-group">
-            <label  class="bmd-label-floating">Programa</label>
-            <input type="text" class="form-control" value={this.props.name}/>
+          <div className="form-group">
+            <label className="bmd-label-floating">Programa</label>
+            <input type="text" className="form-control" value={this.props.name} disabled />
           </div>
-          <div class="form-group">
-            <label class="bmd-label-floating">Fecha</label>
-            <input type="text" class="form-control" value={this.props.day}/>
+          <div className="form-group">
+            <label className="bmd-label-floating">Fecha</label>
+            <input type="text" className="form-control" value={this.props.day} disabled />
           </div>
-          <div class="form-group">
-            <label  class="bmd-label-floating">Monto por producto</label>
-            <input type="text" class="form-control" value="---" />
+          <div className="form-group">
+            <label className="bmd-label-floating">Monto por producto</label>
+            <input type="text" className="form-control" value={this.state.productPrice} disabled />
           </div>
-          <div class="form-group">
-            <label class="bmd-label-floating">Monto por programa</label>
-            <input type="text" class="form-control" value={this.props.price} />
+          <div className="form-group">
+            <label className="bmd-label-floating">Monto por programa</label>
+            <input type="text" className="form-control" value={this.props.price} disabled />
           </div>
-          <div class="form-group">
-            <label  class="bmd-label-floating">Monto por recargo</label>
-            <input type="text" class="form-control" value="---"/>
+          <div className="form-group">
+            <label className="bmd-label-floating">Monto por recargo</label>
+            <input type="text" className="form-control" value={this.state.surchargeAmount} disabled />
           </div>
-          <div class="form-group">
-            <label  class="bmd-label-floating">Monto total</label>
-            <input type="text" class="form-control" value="---"/>
+          <div className="form-group">
+            <label className="bmd-label-floating">Monto total</label>
+            <input type="text" className="form-control" ref={this.myRef} value={(parseInt(this.state.productPrice) + parseInt(this.props.price) + parseInt(this.state.surchargeAmount))} disabled />
           </div>
-          
-          
-          <div class="checkbox">
+          <div className="form-check">
             <label>
-              <input type="checkbox" aria-label="Checkbox for following text input"/> Acepto los términos y condiciones 
+              <input type="checkbox" className="form-check-input" caria-label="Checkbox for following text input" /> Acepto los términos y condiciones
             </label>
           </div>
-          <div>
-            <button type="submit" class="btn btn-raised btn-success btn-login">Enviar</button>
-            <button class="btn btn-outline-secondary btn-login">Cancelar</button>
-          </div>
-          
-         
+          <button type="submit" className="btn btn-raised btn-warning btn-login" name="cancel">Cancelar</button>
+          <button type="submit" className="btn btn-raised btn-success btn-login" name="submit">Enviar</button>
         </form>
       </div>
     );
   }
+  handleChange(e) {
+    const newTarget = e.target.value.split('-');
+    this.setState({ productPrice: parseInt(newTarget[0]), productName: newTarget[1] });
+  }
+  handleSubmit(e) {
+    e.preventDefault();
+    if (this.state.productPrice !== 0) {
+      database.ref('booking/').push({
+        name: this.props.name,
+        productName: this.state.productName,
+        productPrice: this.state.productPrice,
+        programPrice: this.props.price,
+        surchargePrice: this.state.surchargeAmount,
+        totalPrice: this.myRef.current.value
+      });
+    } else {
+      alert("seleccionar producto")
+    }
+  }
 }
-
 export default Form;
