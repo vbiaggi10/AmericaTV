d6166dbc12a5801f212bfc820dac11ad844bd094
vbiaggi10 <vbiaggi10@gmail.com>
2018-10-31 14:21:02 -0500

    Updates

---
+++ precache-manifest.1e99dbd700cc5299113616dadba0f056.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = [
+  {
+    "revision": "0e6c3d7513abea4f39976230e4feeac0",
+    "url": "/static/media/frame.0e6c3d75.svg"
+  },
+  {
+    "revision": "cf040d75637959ef3ebaa81d0fde25eb",
+    "url": "/static/media/fondo3.cf040d75.jpg"
+  },
+  {
+    "revision": "229c360febb4351a89df",
+    "url": "/static/js/runtime~main.229c360f.js"
+  },
+  {
+    "revision": "1169b01448a1a0979adf",
+    "url": "/static/js/main.1169b014.chunk.js"
+  },
+  {
+    "revision": "bc0be2705619fde3cf22",
+    "url": "/static/js/1.bc0be270.chunk.js"
+  },
+  {
+    "revision": "1169b01448a1a0979adf",
+    "url": "/static/css/main.077f63b3.chunk.css"
+  },
+  {
+    "revision": "93015559275af18f44460022485e466d",
+    "url": "/index.html"
+  }
+];
